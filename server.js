7997c45d512809bf24e44f9cfbc13f93fc77af94
ledohuydu171000 <64415130+ledohuydu171000@users.noreply.github.com>
2022-06-11 00:53:27 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-//importing packages
+// importing packages
 const express = require('express');
 const admin = require('firebase-admin');
 const bcrypt = require('bcrypt');
 const path = require('path');
+const nodemailer = require('nodemailer');
 
-//firebase admin setup
+// firebase admin setup
 let serviceAccount = require("./e-com-website-430dd-firebase-adminsdk-x24vj-a4af4649ae.json");
 
 admin.initializeApp({
@@ -13,24 +14,28 @@ admin.initializeApp({
 
 let db = admin.firestore();
 
+// aws config
 const aws = require('aws-sdk');
 const dotenv = require('dotenv');
-const { parse } = require('path');
-const { url } = require('inspector');
 
 dotenv.config();
 
-const region = "us-west-2";
-const bucketName = "ecom-website-dhd";
+// aws parameters
+const region = "region";
+const bucketName = "bucket name";
 const accessKeyId = process.env.AWS_ACCESS_KEY;
 const secretAccessKey = process.env.AWS_SECRET_KEY;
 
 aws.config.update({
-    region, accessKeyId, secretAccessKey
+    region, 
+    accessKeyId, 
+    secretAccessKey
 })
 
+// init s3
 const s3 = new aws.S3();
 
+// generate image upload link
 async function generateUrl(){
     let date = new Date();
     let id = parseInt(Math.random() * 10000000000);
@@ -40,15 +45,16 @@ async function generateUrl(){
     const params = ({
         Bucket: bucketName,
         Key: imageName,
-        Expires: 300,
+        Expires: 300, //300 ms
         ContentType: 'image/jpeg'
     })
     const uploadUrl = await s3.getSignedUrlPromise('putObject', params);
     return uploadUrl;
 }
-//declare stattic path
+
+// declare static path
 let staticPath = path.join(__dirname, "public");
-    
+
 //intializing express.js
 const app = express();
 
@@ -60,37 +66,38 @@ app.use(express.json());
 //home route
 app.get("/", (req, res) => {
     res.sendFile(path.join(staticPath, "index.html"));
-}) 
+})
 
 //signup route
-app.get("/signup", (req, res) => {
+app.get('/signup', (req, res) => {
     res.sendFile(path.join(staticPath, "signup.html"));
 })
 
 app.post('/signup', (req, res) => {
-    let { name, email, password, number, tac, notifications} = req.body;
+    let { name, email, password, number, tac, notification } = req.body;
 
-    //form validation
+    // form validations
     if(name.length < 3){
         return res.json({'alert': 'name must be 3 letters long'});
     } else if(!email.length){
         return res.json({'alert': 'enter your email'});
     } else if(password.length < 8){
-        return res.json({'alert': 'password must be 8 characters long'});
+        return res.json({'alert': 'password should be 8 letters long'});
     } else if(!number.length){
         return res.json({'alert': 'enter your phone number'});
     } else if(!Number(number) || number.length < 10){
         return res.json({'alert': 'invalid number, please enter valid one'});
     } else if(!tac){
-        return res.json({'alert': 'you must agree to our terms and condition'});
-    }
-
-    //store user in db
+        return res.json({'alert': 'you must agree to our terms and conditions'});
+    } 
+    
+    // store user in db
     db.collection('users').doc(email).get()
     .then(user => {
         if(user.exists){
             return res.json({'alert': 'email already exists'});
-        } else {
+        } else{
+            // encrypt the password before storing it.
             bcrypt.genSalt(10, (err, salt) => {
                 bcrypt.hash(password, salt, (err, hash) => {
                     req.body.password = hash;
@@ -99,7 +106,7 @@ app.post('/signup', (req, res) => {
                         res.json({
                             name: req.body.name,
                             email: req.body.email,
-                            seller: req.body.seller, 
+                            seller: req.body.seller,
                         })
                     })
                 })
@@ -108,13 +115,13 @@ app.post('/signup', (req, res) => {
     })
 })
 
-//login route
+// login route
 app.get('/login', (req, res) => {
     res.sendFile(path.join(staticPath, "login.html"));
 })
 
 app.post('/login', (req, res) => {
-    let {email, password} = req.body;
+    let { email, password } = req.body;
 
     if(!email.length || !password.length){
         return res.json({'alert': 'fill all the inputs'})
@@ -122,41 +129,41 @@ app.post('/login', (req, res) => {
 
     db.collection('users').doc(email).get()
     .then(user => {
-        if(!user.exists){// is email do not exists
+        if(!user.exists){ // if email does not exists
             return res.json({'alert': 'log in email does not exists'})
-        } else {
+        } else{
             bcrypt.compare(password, user.data().password, (err, result) => {
                 if(result){
-                   let data = user.data();
-                   return res.json({
-                       name: data.name,
-                       email: data.email, 
-                       seller: data.seller,
-                   })
-                } else {
-                    return res.json({'alert': 'password is incorrect'})
+                    let data = user.data();
+                    return res.json({
+                        name: data.name,
+                        email: data.email,
+                        seller: data.seller,
+                    })
+                } else{
+                    return res.json({'alert': 'password is incorrect'});
                 }
             })
         }
     })
 })
 
-//seller route
+// seller route
 app.get('/seller', (req, res) => {
-    res.sendFile(path.join(staticPath, 'seller.html'));
+    res.sendFile(path.join(staticPath, "seller.html"));
 })
 
 app.post('/seller', (req, res) => {
-    let {name, about, address, number, tac, legit, email} = req.body;
+    let { name, about, address, number, tac, legit, email } = req.body;
     if(!name.length || !address.length || !about.length || number.length < 10 || !Number(number)){
-        return res.json({'alert': 'some information(s) is/are invalid'});
+        return res.json({'alert':'some inforamation(s) is/are invalid'});
     } else if(!tac || !legit){
-        return res.json({'alert': 'you must agree to our terms and conditions'});
-    } else {
-        //update usres seller status here.
-        db.collection('seller').doc(email).set(req.body)
-        .then (data => {
-            db.collection('user').doc(email).update({
+        return res.json({'alert': 'you must agree to our terms and conditions'})
+    } else{
+        // update users seller status here.
+        db.collection('sellers').doc(email).set(req.body)
+        .then(data => {
+            db.collection('users').doc(email).update({
                 seller: true
             }).then(data => {
                 res.json(true);
@@ -165,45 +172,50 @@ app.post('/seller', (req, res) => {
     }
 })
 
-app.get('/add-product',(req,res)=>{
-    res.sendFile(path.join(staticPath,"addProduct.html"));
+// add product
+app.get('/add-product', (req, res) => {
+    res.sendFile(path.join(staticPath, "addProduct.html"));
 })
 
-app.get('/add-product/:id',(req,res)=>{
-    res.sendFile(path.join(staticPath,"addProduct.html"));
+app.get('/add-product/:id', (req, res) => {
+    res.sendFile(path.join(staticPath, "addProduct.html"));
 })
 
+// get the upload link
 app.get('/s3url', (req, res) => {
     generateUrl().then(url => res.json(url));
 })
 
+// add product
 app.post('/add-product', (req, res) => {
     let { name, shortDes, des, images, sizes, actualPrice, discount, sellPrice, stock, tags, tac, email, draft, id } = req.body;
 
+    // validation
     if(!draft){
         if(!name.length){
             return res.json({'alert': 'enter product name'});
-        }else if (shortDes.length >100 || shortDes.length < 10 ){
+        } else if(shortDes.length > 100 || shortDes.length < 10){
             return res.json({'alert': 'short description must be between 10 to 100 letters long'});
-        }else if (!des.length){
+        } else if(!des.length){
             return res.json({'alert': 'enter detail description about the product'});
-        }else if (!images.length){
-            return res.json({'alert': 'upload atleast one product image'});
-        }else if (!sizes.length){
+        } else if(!images.length){ // image link array
+            return res.json({'alert': 'upload atleast one product image'})
+        } else if(!sizes.length){ // size array
             return res.json({'alert': 'select at least one size'});
-        }else if (!actualPrice.length || !discount.length || !sellPrice.length){
+        } else if(!actualPrice.length || !discount.length || !sellPrice.length){
             return res.json({'alert': 'you must add pricings'});
-        }else if(stock < 20){
+        } else if(stock < 20){
             return res.json({'alert': 'you should have at least 20 items in stock'});
-        }else if(!tags.length){
+        } else if(!tags.length){
             return res.json({'alert': 'enter few tags to help ranking your product in search'});
-        }else if(!tac){
+        } else if(!tac){
             return res.json({'alert': 'you must agree to our terms and conditions'});
-        }
+        } 
     }
 
+    // add product
     let docName = id == undefined ? `${name.toLowerCase()}-${Math.floor(Math.random() * 5000)}` : id;
-    db.collection('product').doc(docName).set(req.body)
+    db.collection('products').doc(docName).set(req.body)
     .then(data => {
         res.json({'product': name});
     })
@@ -212,9 +224,17 @@ app.post('/add-product', (req, res) => {
     })
 })
 
+// get products
 app.post('/get-products', (req, res) => {
-    let {email, id} = req.body;
-    let docRef = id ? db.collection('products').doc(id) : db.collection('products').where('email', '==', email);
+    let { email, id, tag } = req.body;
+
+    if(id){
+        docRef = db.collection('products').doc(id)
+    } else if(tag){
+        docRef = db.collection('products').where('tags', 'array-contains', tag)
+    } else{
+        docRef = db.collection('products').where('email', '==', email)
+    }
 
     docRef.get()
     .then(products => {
@@ -224,9 +244,9 @@ app.post('/get-products', (req, res) => {
         let productArr = [];
         if(id){
             return res.json(products.data());
-        }else{
+        } else{
             products.forEach(item => {
-                let data =item.data();
+                let data = item.data();
                 data.id = item.id;
                 productArr.push(data);
             })
@@ -237,7 +257,7 @@ app.post('/get-products', (req, res) => {
 
 app.post('/delete-product', (req, res) => {
     let { id } = req.body;
-
+    
     db.collection('products').doc(id).delete()
     .then(data => {
         res.json('success');
@@ -246,15 +266,118 @@ app.post('/delete-product', (req, res) => {
     })
 })
 
-//404 route
-app.get('/404', (req, res) =>{
+// product page
+app.get('/products/:id', (req, res) => {
+    res.sendFile(path.join(staticPath, "product.html"));
+})
+
+app.get('/search/:key', (req, res) => {
+    res.sendFile(path.join(staticPath, "search.html"));
+})
+
+app.get('/cart', (req, res) => {
+    res.sendFile(path.join(staticPath, "cart.html"));
+})
+
+app.get('/checkout', (req, res) => {
+    res.sendFile(path.join(staticPath, "checkout.html"));
+})
+
+app.post('/order', (req, res) => {
+    const { order, email, add } = req.body;
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+        }
+    })
+
+    const mailOption = {
+        from: 'sender email',
+        to: email,
+        subject: 'Clothing : Order Placed',
+        html: `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+
+            <style>
+                body{
+                    min-height: 90vh;
+                    background: #f5f5f5;
+                    font-family: sans-serif;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                }
+                .heading{
+                    text-align: center;
+                    font-size: 40px;
+                    width: 50%;
+                    display: block;
+                    line-height: 50px;
+                    margin: 30px auto 60px;
+                    text-transform: capitalize;
+                }
+                .heading span{
+                    font-weight: 300;
+                }
+                .btn{
+                    width: 200px;
+                    height: 50px;
+                    border-radius: 5px;
+                    background: #3f3f3f;
+                    color: #fff;
+                    display: block;
+                    margin: auto;
+                    font-size: 18px;
+                    text-transform: capitalize;
+                }
+            </style>
+
+        </head>
+        <body>
+            
+            <div>
+                <h1 class="heading">dear ${email.split('@')[0]}, <span>your order is successfully placed</span></h1>
+                <button class="btn">check status</button>
+            </div>
+
+        </body>
+        </html>
+        `
+    }
+
+    let docName = email + Math.floor(Math.random() * 123719287419824);
+    db.collection('order').doc(docName).set(req.body)
+    .then(data => {
+
+        transporter.sendMail(mailOption, (err, info) => {
+            if(err){
+                res.json({'alert': 'opps! its seems like some err occured. Try again'})
+            } else{
+                res.json({'alert': 'your order is placed', 'type': 'success'});
+            }
+        })
+
+    })
+})
+
+// 404 route
+app.get('/404', (req, res) => {
     res.sendFile(path.join(staticPath, "404.html"));
 })
 
-app.use((req, res) =>{
+app.use((req, res) => {
     res.redirect('/404');
 })
 
 app.listen(3000, () => {
-    console.log('listening on port 3000......');
+    console.log('listening on port 3000.......');
 })
